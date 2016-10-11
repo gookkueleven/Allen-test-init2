aba8032f04c8f53892c0152f4d652bab76fb3c7b
ratchasit.amo <ratchasit.amo@ascendcorp.com>
2016-10-11 17:35:27 +0700

    add pathvariable api

--- src/main/java/kooknaja/controller/HelloWorld.java
+++ src/main/java/kooknaja/controller/HelloWorld.java
@@ -2,7 +2,9 @@ package kooknaja.controller; /**
  * Created by ratchasit.amo on 10/11/2016.
  */
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,4 +14,10 @@ public class HelloWorld{
     public String index(){
         return "Hello, world";
     }
+
+    @RequestMapping(value="/{name}/{last}", method = RequestMethod.GET)
+        String testPath(@PathVariable String name, @PathVariable("last") String l){
+        return "Hello, " + name + l;
+    }
+
 }
